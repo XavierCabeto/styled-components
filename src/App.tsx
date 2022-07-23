ab1395907900fe14636f44df328d28edce44d6c9
XavierCabeto <xaviercabeto@gmail.com>
2022-07-23 11:15:28 +0100

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components';
+
+interface ButtonProps {
+  size: 'big' | 'small';
+}
+
+const Button = styled.button<ButtonProps>`
+  background-color: pink;
+  padding: 12px;
+  border-radius: 8px;
+  border: 1px solid purple;
+
+  ${props => props.size === 'big' && css`
+    font-size: 64px;
+    padding: 48px;
+  `}
+`
+function App() {
+  return (
+    <div>
+      <Button size="big">Hello World</Button>
+      <Button size="small">Hello World</Button>
+    </div>
+  )
+}
+
+export default App
